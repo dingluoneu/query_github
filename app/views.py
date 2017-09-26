e452f2529e2f695f9f7092f30bf87f4e94af4939
dingluoneu <dingluo.bupt@gmail.com>
2017-09-26 19:40:29 -0400

    Demo finished.

--- app/views.py
+++ app/views.py
@@ -13,19 +13,21 @@ def test(request):
 
 
 def profile(request):
-    json_list = []
-    req = requests.get("https://api.github.com/users/dingluoneu")
-    json_list.append(json.loads(req.text))
     parsed_data = []
-    user_data = {}
-    for data in json_list:
-        user_data['name'] = data['name']
-        user_data['blog'] = data['blog']
-        user_data['email'] = data['email']
-        user_data['public_gists'] = data['public_gists']
-        user_data['public_repos'] = data['public_repos']
-        user_data['avatar_url'] = data['avatar_url']
-        user_data['followers'] = data['followers']
-        user_data['following'] = data['following']
-    parsed_data.append(user_data)
-    return HttpResponse(parsed_data)
+    if request.method == 'POST':
+        username = request.POST.get('user')
+        req = requests.get('https://api.github.com/users/' + username)
+        json_list = list()
+        json_list.append(req.json())
+        user_data = {}
+        for data in json_list:
+            user_data['name'] = data['name']
+            user_data['blog'] = data['blog']
+            user_data['email'] = data['email']
+            user_data['public_gists'] = data['public_gists']
+            user_data['public_repos'] = data['public_repos']
+            user_data['avatar_url'] = data['avatar_url']
+            user_data['followers'] = data['followers']
+            user_data['following'] = data['following']
+        parsed_data.append(user_data)
+    return render(request, 'app/profile.html', {'data': parsed_data})
