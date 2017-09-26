64a9ba482aa9ca674046e6926978a378ec84ab29
dingluoneu <dingluo.bupt@gmail.com>
2017-09-26 19:18:28 -0400

    Set up Brower.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import requests
+import json
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello World!")
+
+
+def test(request):
+    return HttpResponse("This is a test!")
+
+
+def profile(request):
+    json_list = []
+    req = requests.get("https://api.github.com/users/dingluoneu")
+    json_list.append(json.loads(req.text))
+    parsed_data = []
+    user_data = {}
+    for data in json_list:
+        user_data['name'] = data['name']
+        user_data['blog'] = data['blog']
+        user_data['email'] = data['email']
+        user_data['public_gists'] = data['public_gists']
+        user_data['public_repos'] = data['public_repos']
+        user_data['avatar_url'] = data['avatar_url']
+        user_data['followers'] = data['followers']
+        user_data['following'] = data['following']
+    parsed_data.append(user_data)
+    return HttpResponse(parsed_data)
